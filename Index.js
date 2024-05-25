596d35ac0450afb37abe78a17633d15f35fcefb7
Lyn-le <babylyndeguzman54@gmail.com>
2024-05-25 18:13:22 +0800

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,8 @@
+const btnlike1=document.getelementByid("btnlike1")
+const countlike1=document.getelementByid("countlike1")
+
+function clicklike1(){
+  let totallikes= parseInt(countlike1.value)+ 1
+  countlike1.textcontent=totallikes.toString()
+}
+function(){
